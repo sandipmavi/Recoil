6eaad02a288438a5e1006ca2fe6d460a5eff72d5
sandipmavi <gurjarsandeep9897@gmail.com>
2025-08-01 05:37:29 +0530

    Learning Recoil

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { RecoilRoot, atom, useRecoilValue, useSetRecoilState } from "recoil";
+
+const count = atom({
+  key: "countState",
+  default: 0,
+});
+
+function App() {
+  return (
+    <RecoilRoot>
+      <Counter />
+    </RecoilRoot>
+  );
+}
+function Counter() {
+  return (
+    <>
+      <Increase />
+      <Decrease />
+      <CurCount />
+    </>
+  );
+}
+
+function Decrease() {
+  const setCount = useSetRecoilState(count);
+  return <button onClick={() => setCount((c) => c - 1)}>Decrease</button>;
+}
+function Increase() {
+  const setCount = useSetRecoilState(count);
+  return <button onClick={() => setCount((c) => c + 1)}>Increase</button>;
+}
+function CurCount() {
+  const countValue = useRecoilValue(count);
+  return <div>{countValue}</div>;
+}
+
+export default App;
